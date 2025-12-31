b83b1b2c3b48647398b0aa1d07af4f8dc93416a7
Harish-189 <harishsundarm.cb24@bitsathy.ac.in>
2025-12-31 09:26:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+// Mobile Menu
+const hamburger = document.querySelector(".hamburger");
+const navLinks = document.querySelector(".nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("active");
+});
+
+// Scroll Animation
+const faders = document.querySelectorAll(".fade-in");
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    }
+  });
+});
+
+faders.forEach(el => observer.observe(el));
